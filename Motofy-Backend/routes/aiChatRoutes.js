f4b8f7bb0d9f57972c8db602f4c193d1c528bb17
Trishna <your-github-trishnarr46@gmail.com>
2025-09-07 16:55:52 +0530

    Updated backend files for deployment fixes

--- Motofy-Backend/routes/aiChatRoutes.js
+++ Motofy-Backend/routes/aiChatRoutes.js
@@ -6,5 +6,4 @@ const { askAI } = require('../controllers/aiChatController');
 
 // POST: Ask AI a question
 router.post('/', askAI);
-router.post('https://motofy-l5gq.onrender.com/api/ai-chat', askAI);
 module.exports = router;
